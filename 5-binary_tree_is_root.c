ea4b77105be457c713e087276bd6f3f6121e59d7
amineNouabi <aminenouabi9@gmail.com>
2024-06-04 19:50:31 +0100

    Is Root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_is_leaf - checks if a node is root
+ * @node: node to check
+ *
+ * Return: void
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (!node->parent)
+		return (1);
+
+	return (0);
+}
