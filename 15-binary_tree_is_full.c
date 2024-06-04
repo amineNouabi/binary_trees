5562dc123cb296264ecb50e51e900a04b1e8e9db
amineNouabi <aminenouabi9@gmail.com>
2024-06-04 21:39:58 +0100

    Binary tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,7 +8,10 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
 		return (1);
 
 	if (!tree->left || !tree->right)
