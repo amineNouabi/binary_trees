bd5845965259cfa805dd42955dfdabd5fe246f02
amineNouabi <aminenouabi9@gmail.com>
2024-06-04 20:06:11 +0100

    in and post order added

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,7 +12,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
-	func(tree);
+	func(tree->n);
 
 	if (tree->left)
 		binary_tree_preorder(tree->left, func);
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -20,5 +20,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree->right)
 		binary_tree_postorder(tree->right, func);
 
-	func(tree);
+	func(tree->n);
 }
